81ecf9f031728d617330daef771a8dac519e9a52
sanfusu <sanfusu@outlook.com>
2021-07-25 22:00:40 +0800

    add bitindex just like sliceindex

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,71 @@
 use std::ops::RangeInclusive;
 
+pub trait BitIndex {
+    fn offset(&self) -> u32;
+    fn len(&self) -> u32;
+}
+
+impl BitIndex for RangeInclusive<u32> {
+    fn offset(&self) -> u32 {
+        *self.start()
+    }
+
+    fn len(&self) -> u32 {
+        self.end() - self.start()
+    }
+}
+impl BitIndex for u32 {
+    fn offset(&self) -> u32 {
+        *self
+    }
+
+    fn len(&self) -> u32 {
+        1
+    }
+}
+
 pub trait BitOps
 where
     Self: Sized,
 {
-    fn bits_set(self, range: RangeInclusive<u32>) -> Self;
-    fn bits_clr(self, range: RangeInclusive<u32>) -> Self;
-    fn bits_revert(self, range: RangeInclusive<u32>) -> Self;
-    fn bits_write(self, range: RangeInclusive<u32>, value: Self) -> Self;
-    fn bits_read(self, range: RangeInclusive<u32>) -> Self;
+    fn bits_set<T>(self, range: T) -> Self
+    where
+        T: BitIndex;
+    fn bits_clr<T: BitIndex>(self, range: T) -> Self;
+    fn bits_revert<T: BitIndex>(self, range: T) -> Self;
+    fn bits_write<T: BitIndex>(self, range: T, value: Self) -> Self;
+    fn bits_read<T: BitIndex>(self, range: T) -> Self;
 }
-
+macro_rules! mask {
+    ($Type:ty, $Range:expr) => {
+        match (1 as $Type).checked_shl($Range.len()) {
+            Some(value) => value - 1,
+            None => <$Type>::MAX,
+        } << $Range.offset()
+    };
+}
 macro_rules! impl_bitops {
     ($($Type:ty) *) => {
         $(impl BitOps for $Type {
-            fn bits_set(self, range: RangeInclusive<u32>) -> Self {
-                let mask = match (1 as $Type).checked_shl(range.end() - range.start()){
-                    Some(value) => value - 1,
-                    None => <$Type>::MAX,
-                };
+            fn bits_set<T:BitIndex>(self, range:T) -> Self {
+                let mask = mask!($Type, range);
                 self | mask
             }
-            fn bits_clr(self, range: RangeInclusive<u32>) -> Self {
-                let mask = 1 << (range.end() - range.start()) - 1;
+            fn bits_clr<T:BitIndex>(self, range: T) -> Self {
+                let mask = mask!($Type, range);
                 self & (!mask)
             }
-            fn bits_revert(self, range: RangeInclusive<u32>) -> Self {
-                let mask = 1 << (range.end() - range.start()) - 1;
+            fn bits_revert<T:BitIndex>(self, range: T) -> Self {
+                let mask = mask!($Type, range);
                 self ^ mask
             }
-            fn bits_write(self, range: RangeInclusive<u32>, value: Self) -> Self {
-                let mask = 1 << (range.end() - range.start()) - 1;
+            fn bits_write<T:BitIndex>(self, range:T, value: Self) -> Self {
+                let mask = mask!($Type, range);
                 self & (!mask) | value
             }
-            fn bits_read(self, range: RangeInclusive<u32>) -> Self {
-                let mask = 1 << (range.end() - range.start()) - 1;
-                (self & mask) >> range.start()
+            fn bits_read<T:BitIndex>(self, range: T) -> Self {
+                let mask = mask!($Type, range);
+                (self & mask) >> range.offset()
             }
         })*
     };
